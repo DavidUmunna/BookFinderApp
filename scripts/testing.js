be72fa92f39ea954d5feb2171fe0e715315fdada
David Umunna <66871488+DavidUmunna@users.noreply.github.com>
2025-02-21 12:56:21 +0100

    Update testing.js

--- scripts/testing.js
+++ scripts/testing.js
@@ -1,4 +1,4 @@
-fetch("https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=AIzaSyBEPQ99SQ0ptUvFwxmotMW1bAJK68LO6yc")
+fetch("https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=")
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error("Error fetching books:", error));
